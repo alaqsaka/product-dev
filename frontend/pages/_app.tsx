82c194eac2d27ca604788091b2d5dc8f0eb0fae6
alaqsaka <aqsaabidin15@gmail.com>
2023-10-07 14:22:29 +0700

    Creating layout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,13 @@
+import Layout from '@/components/layout';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+  <Layout>
+      <Component {...pageProps} />
+  </Layout>
   );
 }
 
