ccd6b3938f6258534ae1474ca8a9d5b699b4b81a
alaqsaka <aqsaabidin15@gmail.com>
2023-10-09 05:27:39 +0700

    user loggeding profile

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,11 +1,40 @@
-import React, { useEffect } from 'react'
-import Button from './button'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import Button from './button';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = ({token}: {token: string}) => {
-  useEffect(() => {
+  const [profileData, setProfileData] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
-  }, []);
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      const response = await fetch(`https://api.github.com/user`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.json();
+      setProfileData(result);
+
+      console.log('Fetch Profile Data ', result);
+      setLoading(false);
+    }
+
+
+    fetchProfileData()
+    .catch((e) => {
+      console.log('Error ',e);
+      console.error('An error occurred while fetching the data: ', e)
+      setLoading(false);
+    })
+    setLoading(true);
+    console.log('loading ',loading);
+  }, [token]);
+
 
 
   return (
@@ -16,7 +45,11 @@ const Navbar = ({token}: {token: string}) => {
 
       {
         token ? <>
-          User
+          {/* {profileData?.avatar_url} */}
+          <div className='border rounded-full px-4 py-2 flex h-[56px] w-[112px] gap-4' style={{ borderColor: '#D0D5DD' }}>
+          <Image className='rounded-full' src={profileData?.avatar_url} height={40} width={40} alt='Profile Picture'/>
+          <Image src={'/burger.svg'} height={24} width={24} alt='Dropdown'/>
+          </div>
         </> : (
           <Link href={`/api/github/auth`}>
             <Button variant='primary'>
